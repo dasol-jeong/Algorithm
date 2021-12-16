7c1aed4758d97c64682f287ff30d2aa0091bdea9
dasol-jeong <huiogc1214@gmail.com>
2021-12-17 00:29:39 +0900

    feat: leetcode3 longest substring without repeating characters

--- 문제/leetcode3.js
+++ 문제/leetcode3.js
@@ -5,10 +5,10 @@ function solution(s) {
   const map = {};
   let left = 0;
 
-  return s.split('').reduce((max,v,i)=>){
-    left=map[v] >= left ? map[v]+1:left;
-    map[v]=i;
-    return Math.max(max,i-left+1);
-  },0);
+  return s.split("").reduce((max, v, i) => {
+    left = map[v] >= left ? map[v] + 1 : left;
+    map[v] = i;
+    return Math.max(max, i - left + 1);
+  }, 0);
 }
 console.log(solution("abcabcbb"));
